ab00fc0f9b1bc0c8e5529ecd8fd1683b63a7e51e
Alex <alex.chenqr@gmail.com>
2024-05-12 16:29:07 +0800

    add FlashLoanSimpleReceiverBase

--- contracts/DoFlashLoan.sol
+++ contracts/DoFlashLoan.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-contract DoFlashLoan {
+import "https://github.com/aave/aave-v3-core/blob/master/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+
+contract DoFlashLoan is FlashLoanSimpleReceiverBase{
 
 }
