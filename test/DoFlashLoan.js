28ceb6958c58e7bf72d59718160427aa57fd75ce
Alex <alex.chenqr@gmail.com>
2024-05-12 17:17:02 +0800

    add

--- test/DoFlashLoan.js
+++ test/DoFlashLoan.js
@@ -5,5 +5,7 @@ describe("Test DoFlashLoan", function() {
 
     async function deployTokenFixture() {
         const DoFlashLoan = await ethers.getContractFactory("DoFlashLoan");
+
+        console.log(process.env.POOL_ADDR_PROVIDER)
     }
 });
