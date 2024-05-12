a8c261bde37cfa689d5ba2ec5f0b6021da495877
Alex <alex.chenqr@gmail.com>
2024-05-12 17:09:49 +0800

    add

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  
+  const doFlashLoan = await hre.ethers.deployContract("DoFlashLoan");
+
+  await doFlashLoan.waitForDeployment();
+  console.log(
+    `RouletteGame deployed to ${doFlashLoan.target}`
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
