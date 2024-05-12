99b7d73e2bf2a77fb0650d8699b6f5bf877ba16c
Alex <alex.chenqr@gmail.com>
2024-05-12 21:52:49 +0800

    debug

--- test/DoFlashLoan.js
+++ test/DoFlashLoan.js
@@ -24,9 +24,9 @@ describe("测试DoFlashLoan合约逻辑", function() {
         
         const aToken = process.env.A_TOKEN;
         const addr = process.env.OWNER_ADDRESS;
-        
+
         await doFlashLoan.connect(addr).requireFlashLoan(aToken, 100, 
-            {gasLimit: 200000, gasPrice: ethers.utils.parseUnits('20', 'gwei')}
+            {gasLimit: 200000, gasPrice: ethers.parseUnit('20', 'gwei')}
         );
     });
 });
