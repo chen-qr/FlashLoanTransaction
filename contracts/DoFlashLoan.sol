3cca1248aee6ddd7d333f5292684e77d1ade285b
Alex <alex.chenqr@gmail.com>
2024-05-12 17:02:22 +0800

    add

--- contracts/DoFlashLoan.sol
+++ contracts/DoFlashLoan.sol
@@ -5,7 +5,7 @@ import { FlashLoanSimpleReceiverBase } from "@aave/core-v3/contracts/flashloan/b
 import { IPoolAddressesProvider } from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import { IERC20 } from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 
-contract DoFlashLoan is FlashLoanSimpleReceiverBase{
+contract DoFlashLoan is FlashLoanSimpleReceiverBase {
 
     constructor(address _addressProvider) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {
 
@@ -19,11 +19,11 @@ contract DoFlashLoan is FlashLoanSimpleReceiverBase{
         bytes calldata params
     ) external override returns (bool) {
         
-        // 处理贷款的逻辑
+        // 接受到贷款后，处理贷款的使用逻辑，用贷款赚钱
         
+        // 贷款使用逻辑执行完后，执行还款逻辑
         uint256 totalAmount = amount + premium;
         IERC20(asset).approve(address(POOL), totalAmount);
-
         return true;
     }
 }
