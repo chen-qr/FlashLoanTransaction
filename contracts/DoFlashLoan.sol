b24eeec7b6d724f9dcfb08219a4196ce48739c8a
Alex <alex.chenqr@gmail.com>
2024-05-12 16:40:06 +0800

    add

--- contracts/DoFlashLoan.sol
+++ contracts/DoFlashLoan.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-import { FlashLoanSimpleReceiverBase } from "@aave/protocol-v2/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import { FlashLoanSimpleReceiverBase } from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
 
 contract DoFlashLoan is FlashLoanSimpleReceiverBase{
 
